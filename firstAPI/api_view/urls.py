4133603a72cf3480c5f3138d944d2f60e120a133
cani <srballesterpez@hotmail.com>
2023-06-11 19:26:30 +0200

    First commit

---
+++ firstAPI/api_view/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('people_in_group/<uuid:group_id>', views.api_home),
+    path('persons/', views.PersonListView.as_view()),
+    path('person/', views.PersonView.as_view()),
+    path('person/<uuid:id>', views.PersonView.as_view()),
+    path('groups/', views.GroupListView.as_view()),
+    path('group/', views.GroupView.as_view()),
+    path('group/<uuid:id>', views.GroupView.as_view())
+]
---
+++ firstAPI/api_view/views.py
@@ -0,0 +1,49 @@
+import json
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+from .models import Person, Group
+from rest_framework.generics import ListAPIView, GenericAPIView, get_object_or_404
+from .serializer import PersonSerializer, GroupSerializer
+class PersonListView(ListAPIView):
+    model = Person
+    serializer_class = PersonSerializer
+    queryset = Person.objects.all()
+
+class PersonView(RetrieveModelMixin, ListModelMixin, DestroyModelMixin, CreateModelMixin, UpdateModelMixin, GenericAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    lookup_field = 'id'
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, *kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class GroupListView(ListAPIView):
+    model = Person
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+
+class GroupView(RetrieveModelMixin, ListModelMixin, DestroyModelMixin, CreateModelMixin, UpdateModelMixin, GenericAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    lookup_field = 'id'
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, *kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+def api_home(request, group_id):
+    queryset = Person.objects.filter(group = group_id)
+    return JsonResponse(PersonSerializer(queryset, many=True).data, safe=False)
+
